8b369bc35a461bcad997b0aa8f5d50df498d1ce3
Parker Beck <parkerebeckjunk@gmail.com>
2024-05-31 13:15:41 -0500

    Test

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import datetime
 
 def main():
     with open("hello.txt", "w") as f:
-        f.write(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S") + ": Hello")
+        f.write(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S") + ": Hello World")
         f.close()
     
 if __name__ == "__main__":
